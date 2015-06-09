8e308913e59f789a86b8377a31f3162a08b50f56
Sean McArthur <sean.monstar@gmail.com>
2015-06-08 18:24:06 -0700

    typo

--- .travis_after.sh
+++ .travis_after.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-if [ $TRAVIS_RUST_VERSION = stable ]; then
+if [[ $TRAVIS_RUST_VERSION = stable ]]; then
   wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
   tar xzf master.tar.gz
   mkdir kcov-master/build
@@ -10,7 +10,7 @@ if [ $TRAVIS_RUST_VERSION = stable ]; then
   make install DESTDIR=../tmp
   cd ../..
   ./kcov-master/tmp/usr/local/bin/kcov --coveralls-id=$TRAVIS_JOB_ID --exclude-pattern=/.cargo target/kcov target/debug/httparse-*
-  if [ $TRAVIS_BRANCH = master && $TRAVIS_PULL_REQUEST = false ]; then
+  if [[ $TRAVIS_BRANCH = master && $TRAVIS_PULL_REQUEST = false ]]; then
     cargo doc --no-deps
     echo "<meta http-equiv=refresh content=0;url=httparse/index.html>" > target/doc/index.html
     pip install --user ghp-import &&
