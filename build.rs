1e2dfe8658339f801ca0983247813f15039543ea
Sean McArthur <sean@seanmonstar.com>
2018-06-21 15:30:33 -0700

    use faster SIMD runtime cache, but detect target_feature in build script

--- build.rs
+++ build.rs
@@ -20,7 +20,7 @@ fn enable_new_features(raw_version: &str) {
     let version = match Version::parse(raw_version) {
         Ok(version) => version,
         Err(err) => {
-            eprintln!("failed to parse `rustc --version`: {}", err);
+            println!("cargo:warning=failed to parse `rustc --version`: {}", err);
             return;
         }
     };
@@ -31,7 +31,7 @@ fn enable_new_features(raw_version: &str) {
 fn enable_simd(version: Version) {
     let env_disable = "CARGO_CFG_HTTPARSE_DISABLE_SIMD";
     if env::var_os(env_disable).is_some() {
-        eprintln!("detected {} environment variable, disabling SIMD", env_disable);
+        println!("cargo:warning=detected {} environment variable, disabling SIMD", env_disable);
         return;
     }
 
@@ -43,6 +43,54 @@ fn enable_simd(version: Version) {
 
     if version >= min_simd_version {
         println!("cargo:rustc-cfg=httparse_simd");
+    }
+
+    // cfg(target_feature) isn't stable yet, but CARGO_CFG_TARGET_FEATURE has
+    // a list... We aren't doing anything unsafe, since the is_x86_feature_detected
+    // macro still checks in the actual lib, BUT!
+    //
+    // By peeking at the list here, we can change up slightly how we do feature
+    // detection in the lib. If our features aren't in the feature list, we
+    // stick with a cached runtime detection strategy.
+    //
+    // But if the features *are* in the list, we benefit from removing our cache,
+    // since the compiler will eliminate several branches with its internal
+    // cfg(target_feature) usage.
+
+
+    let env_runtime_only = "CARGO_CFG_HTTPARSE_DISABLE_SIMD_COMPILETIME";
+    if env::var_os(env_runtime_only).is_some() {
+        println!("cargo:warning=detected {} environment variable, using runtime SIMD detection only", env_runtime_only);
+        return;
+    }
+    let feature_list = match env::var_os("CARGO_CFG_TARGET_FEATURE") {
+        Some(var) => match var.into_string() {
+            Ok(s) => s,
+            Err(_) => {
+                println!("cargo:warning=CARGO_CFG_TARGET_FEATURE was not valid utf-8");
+                return;
+            },
+        },
+        None => {
+            println!("cargo:warning=CARGO_CFG_TARGET_FEATURE was not set");
+            return
+        },
+    };
+
+    let mut saw_sse42 = false;
+    let mut saw_avx2 = false;
+
+    for feature in feature_list.split(',') {
+        let feature = feature.trim();
+        if !saw_sse42 && feature == "sse4.2" {
+            saw_sse42 = true;
+            println!("cargo:rustc-cfg=httparse_simd_target_feature_sse42");
+        }
+
+        if !saw_avx2 && feature == "avx2" {
+            saw_avx2 = true;
+            println!("cargo:rustc-cfg=httparse_simd_target_feature_avx2");
+        }
     }
 }
 
--- src/simd/mod.rs
+++ src/simd/mod.rs
@@ -28,6 +28,10 @@ mod sse42;
 #[cfg(all(
     httparse_simd,
     any(
+        httparse_simd_target_feature_avx2,
+        not(httparse_simd_target_feature_sse42),
+    ),
+    any(
         target_arch = "x86",
         target_arch = "x86_64",
     ),
@@ -36,13 +40,65 @@ mod avx2;
 
 #[cfg(all(
     httparse_simd,
+    not(any(
+        httparse_simd_target_feature_sse42,
+        httparse_simd_target_feature_avx2,
+    )),
     any(
         target_arch = "x86",
         target_arch = "x86_64",
     ),
 ))]
-mod vector {
+mod runtime {
+    //! Runtime detection of simd features. Used when the build script
+    //! doesn't notice any target features at build time.
+    //!
+    //! While `is_x86_feature_detected!` has it's own caching built-in,
+    //! at least in 1.27.0, the functions don't inline, leaving using it
+    //! actually *slower* than just using the scalar fallback.
+
+    use std::sync::atomic::{AtomicUsize, ATOMIC_USIZE_INIT, Ordering};
+
+    static FEATURE: AtomicUsize = ATOMIC_USIZE_INIT;
+
+    const INIT: usize = 0;
+    const SSE_42: usize = 1;
+    const AVX_2: usize = 2;
+    const NONE: usize = ::std::usize::MAX;
+
+    fn detect() -> usize {
+        let feat = FEATURE.load(Ordering::Relaxed);
+        if feat == INIT {
+            if is_x86_feature_detected!("avx2") {
+                FEATURE.store(AVX_2, Ordering::Relaxed);
+                return AVX_2;
+            } else if is_x86_feature_detected!("sse4.2") {
+                FEATURE.store(SSE_42, Ordering::Relaxed);
+                return SSE_42;
+            } else {
+                FEATURE.store(NONE, Ordering::Relaxed);
+            }
+        }
+        feat
+    }
+
     pub fn match_uri_vectored(bytes: &mut ::Bytes) {
+        unsafe {
+            match detect() {
+                SSE_42 => super::sse42::parse_uri_batch_16(bytes),
+                AVX_2 => super::avx2::parse_uri_batch_32(bytes),
+                _ => ()
+            }
+        }
+
+        /*
+        if is_x86_feature_detected!("sse4.2") {
+            unsafe {
+                super::sse42::parse_uri_batch_16(bytes);
+            }
+        }
+        */
+        /*
         if is_x86_feature_detected!("avx2") {
             unsafe {
                 super::avx2::parse_uri_batch_32(bytes);
@@ -53,16 +109,77 @@ mod vector {
                 super::sse42::parse_uri_batch_16(bytes);
             }
         }
+        */
 
         // else do nothing
     }
 
     pub fn match_header_value_vectored(bytes: &mut ::Bytes) {
+        unsafe {
+            match detect() {
+                SSE_42 => super::sse42::match_header_value_batch_16(bytes),
+                AVX_2 => super::avx2::match_header_value_batch_32(bytes),
+                _ => ()
+            }
+        }
+        /*
+        if is_x86_feature_detected!("sse4.2") {
+            unsafe {
+                super::sse42::match_header_value_batch_16(bytes);
+            }
+        }
+        */
+        /*
         if is_x86_feature_detected!("avx2") {
             unsafe {
                 super::avx2::match_header_value_batch_32(bytes);
             }
         } else if is_x86_feature_detected!("sse4.2") {
+            unsafe {
+                super::sse42::match_header_value_batch_16(bytes);
+            }
+        }
+        */
+
+        // else do nothing
+    }
+}
+
+#[cfg(all(
+    httparse_simd,
+    not(any(
+        httparse_simd_target_feature_sse42,
+        httparse_simd_target_feature_avx2,
+    )),
+    any(
+        target_arch = "x86",
+        target_arch = "x86_64",
+    ),
+))]
+pub use self::runtime::*;
+
+#[cfg(all(
+    httparse_simd,
+    httparse_simd_target_feature_sse42,
+    not(httparse_simd_target_feature_avx2),
+    any(
+        target_arch = "x86",
+        target_arch = "x86_64",
+    ),
+))]
+mod sse42_compile_time {
+    pub fn match_uri_vectored(bytes: &mut ::Bytes) {
+        if is_x86_feature_detected!("sse4.2") {
+            unsafe {
+                super::sse42::parse_uri_batch_16(bytes);
+            }
+        }
+
+        // else do nothing
+    }
+
+    pub fn match_header_value_vectored(bytes: &mut ::Bytes) {
+        if is_x86_feature_detected!("sse4.2") {
             unsafe {
                 super::sse42::match_header_value_batch_16(bytes);
             }
@@ -74,9 +191,64 @@ mod vector {
 
 #[cfg(all(
     httparse_simd,
+    httparse_simd_target_feature_sse42,
+    not(httparse_simd_target_feature_avx2),
     any(
         target_arch = "x86",
         target_arch = "x86_64",
     ),
 ))]
-pub use self::vector::*;
+pub use self::sse42_compile_time::*;
+
+#[cfg(all(
+    httparse_simd,
+    httparse_simd_target_feature_avx2,
+    any(
+        target_arch = "x86",
+        target_arch = "x86_64",
+    ),
+))]
+mod avx2_compile_time {
+    pub fn match_uri_vectored(bytes: &mut ::Bytes) {
+        // do both, since avx2 only works when bytes.len() >= 32
+        if is_x86_feature_detected!("avx2") {
+            unsafe {
+                super::avx2::parse_uri_batch_32(bytes);
+            }
+
+        } 
+        if is_x86_feature_detected!("sse4.2") {
+            unsafe {
+                super::sse42::parse_uri_batch_16(bytes);
+            }
+        }
+
+        // else do nothing
+    }
+
+    pub fn match_header_value_vectored(bytes: &mut ::Bytes) {
+        // do both, since avx2 only works when bytes.len() >= 32
+        if is_x86_feature_detected!("avx2") {
+            unsafe {
+                super::avx2::match_header_value_batch_32(bytes);
+            }
+        }
+        if is_x86_feature_detected!("sse4.2") {
+            unsafe {
+                super::sse42::match_header_value_batch_16(bytes);
+            }
+        }
+
+        // else do nothing
+    }
+}
+
+#[cfg(all(
+    httparse_simd,
+    httparse_simd_target_feature_avx2,
+    any(
+        target_arch = "x86",
+        target_arch = "x86_64",
+    ),
+))]
+pub use self::avx2_compile_time::*;
