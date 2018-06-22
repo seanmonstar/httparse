4f7c5a3453c8eb0bc702a35f4e1f5cefd570490c
Sean McArthur <sean@seanmonstar.com>
2018-06-21 18:42:53 -0700

    update docs about simd usage

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,15 @@
 //! parsing internals use an `Iterator` instead of direct indexing, while
 //! skipping bounds checks.
 //!
-//! The speed is faster than picohttpparser, when SIMD is not available.
+//! With Rust 1.27.0 or later, support for SIMD is enabled automatically.
+//! If building an executable to be run on multiple platforms, and thus
+//! not passing `target_feature` or `target_cpu` flags to the compiler,
+//! runtime detection can still detect SSE4.2 or AVX2 support to provide
+//! massive wins.
+//!
+//! If compiling for a specific target, remembering to include
+//! `-C target_cpu=native` allows the detection to become compile time checks,
+//! making it *even* faster.
 #[cfg(feature = "std")]
 extern crate std as core;
 
