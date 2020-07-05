7a7d9af6adab0983c335129f52066fea0499eea6
Christopher Head <chead@chead.ca>
2020-07-05 15:52:01 -0700

    Unit test Bytes::next_8

--- src/iter.rs
+++ src/iter.rs
@@ -154,3 +154,63 @@ impl<'a, 'b: 'a> Bytes8<'a, 'b> {
         self.pos += 1;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Bytes;
+
+    #[test]
+    fn test_next_8_too_short() {
+        // Start with 10 bytes.
+        let slice = [0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8, 7u8, 8u8, 9u8];
+        let mut bytes = Bytes::new(&slice);
+        // Skip 3 of them.
+        unsafe { bytes.advance(3); }
+        // There should be 7 left, not enough to call next_8.
+        assert!(bytes.next_8().is_none());
+    }
+
+    #[test]
+    fn test_next_8_just_right() {
+        // Start with 10 bytes.
+        let slice = [0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8, 7u8, 8u8, 9u8];
+        let mut bytes = Bytes::new(&slice);
+        // Skip 2 of them.
+        unsafe { bytes.advance(2); }
+        // There should be 8 left, just enough to call next_8.
+        let ret = bytes.next_8();
+        assert!(ret.is_some());
+        let mut ret = ret.unwrap();
+        // They should be the bytes starting with 2.
+        assert_eq!(ret._0(), 2u8);
+        assert_eq!(ret._1(), 3u8);
+        assert_eq!(ret._2(), 4u8);
+        assert_eq!(ret._3(), 5u8);
+        assert_eq!(ret._4(), 6u8);
+        assert_eq!(ret._5(), 7u8);
+        assert_eq!(ret._6(), 8u8);
+        assert_eq!(ret._7(), 9u8);
+    }
+
+    #[test]
+    fn test_next_8_extra() {
+        // Start with 10 bytes.
+        let slice = [0u8, 1u8, 2u8, 3u8, 4u8, 5u8, 6u8, 7u8, 8u8, 9u8];
+        let mut bytes = Bytes::new(&slice);
+        // Skip 1 of them.
+        unsafe { bytes.advance(1); }
+        // There should be 9 left, more than enough to call next_8.
+        let ret = bytes.next_8();
+        assert!(ret.is_some());
+        let mut ret = ret.unwrap();
+        // They should be the bytes starting with 1.
+        assert_eq!(ret._0(), 1u8);
+        assert_eq!(ret._1(), 2u8);
+        assert_eq!(ret._2(), 3u8);
+        assert_eq!(ret._3(), 4u8);
+        assert_eq!(ret._4(), 5u8);
+        assert_eq!(ret._5(), 6u8);
+        assert_eq!(ret._6(), 7u8);
+        assert_eq!(ret._7(), 8u8);
+    }
+}
